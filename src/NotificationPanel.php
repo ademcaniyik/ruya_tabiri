d32dd3263eab29277946aa7cd19c8b8325469283
ademcan <ademcaniyi7@gmail.com>
2025-05-26 21:49:26 +0300

    notif panel

--- src/NotificationPanel.php
+++ src/NotificationPanel.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
 //olacak inş
+//olacak inş
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $selectedTokens = isset($_POST['device_tokens']) ? $_POST['device_tokens'] : [];
